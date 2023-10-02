950668ed9a1808926a8cdae5b74799b1b9d65d7b
Henning Pohl <HenningPohl@gmail.com>
2023-10-02 14:28:53 +0200

    Script for demographics

---
+++ Study Analysis/Demographics.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+dem <- read_csv('demographics.csv') |>
+  filter(Status == 'APPROVED') |>
+  filter(`Participant id` != '611666722763d407c769eb9b') # technical difficulties, didn't actually do study
+
+dem |>
+  mutate(Age = as.numeric(Age)) |>
+  summarize(mean=mean(Age), min=min(Age), max=max(Age))
+
+dem |>
+  group_by(Sex) |>
+  count()
+
+
+dem |>
+  group_by(`Ethnicity simplified`) |>
+  count()
+
+dem |>
+  group_by(`Country of residence`) |>
+  count()
