9570e4ad67d24702198530499b0d9ee2fe8a20f2
Henning Pohl <HenningPohl@gmail.com>
2023-10-02 21:10:15 +0200

    Final study and demographics data

--- Study Analysis/Demographics.R
+++ Study Analysis/Demographics.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 
-dem <- read_csv('demographics.csv') |>
+dem <- read_csv('Demographics.csv') |>
   filter(Status == 'APPROVED') |>
   filter(`Participant id` != '611666722763d407c769eb9b') # technical difficulties, didn't actually do study
 
 dem |>
   mutate(Age = as.numeric(Age)) |>
-  summarize(mean=mean(Age), min=min(Age), max=max(Age))
+  summarize(mean=mean(Age), sd=sd(Age), min=min(Age), max=max(Age))
 
 dem |>
   group_by(Sex) |>
