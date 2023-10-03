08d86e44e2ebdec364d6b52b66bf3db82e97463d
Henning Pohl <HenningPohl@gmail.com>
2023-10-03 16:18:17 +0200

    Alternative comment presentation format

---
+++ Study Analysis/Comments2.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from jinja2 import Template
+
+template = Template('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Participant Comments</title>
+<style>
+body { max-width:800px; margin: auto; }
+.block { page-break-inside: avoid; }
+dt { font-weight: bold; }
+dd { margin: 0 0 0.5em 0; padding: 0 0 0.5em 0; }
+</style>
+</head>
+<body>
+
+{% for question, data in comments %}
+
+<div class="block">
+<h2>{{ question }}</h2>
+
+<ul>
+{% for _, row in data.iterrows() %}
+<li>{{row['Response']}} <i>({{row['Prolific']}})</i></li>
+{% endfor %}
+</ul>
+
+</div>
+
+{% endfor %}
+
+</body>
+</html>
+''')
+
+comments = pd.read_csv('Questionnaire.csv')
+comments = comments.filter(regex=r'Prolific|\?')
+comments = comments.rename(columns={'Please enter your Prolific ID': 'Prolific'})
+
+comments = pd.melt(comments, id_vars='Prolific', value_name='Response', var_name='Question')
+
+with open('Comments2.html', 'w', encoding='utf8') as f:
+    output = template.render(comments=comments.groupby('Question'))
+    f.write(output)
+
